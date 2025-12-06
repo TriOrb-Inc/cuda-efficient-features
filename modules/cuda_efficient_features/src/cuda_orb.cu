6dfd53bdf66733dd6c7380576f8ecc1536e30c94
TOBETA <35523848+MasakazuTobeta@users.noreply.github.com>
2025-12-06 17:52:17 +0900

    Remove blocking sync from CUDA ORB descriptor

--- modules/cuda_efficient_features/src/cuda_orb.cu
+++ modules/cuda_efficient_features/src/cuda_orb.cu
@@ -257,7 +257,6 @@ void computeORB(const GpuMat &integral, const GpuMat &keypoints, GpuMat &descrip
                 integral.cols - 1, integral.rows - 1, keypoints.ptr<float4>(), keypoints.rows, descriptors.ptr<unsigned char>(),
                 static_cast<int>(descriptors.step), scaleFactor, paramSize, patchSize.width, wrapHorizontal);
         CUDA_CHECK(cudaGetLastError());
-        CUDA_CHECK(cudaStreamSynchronize(stream));
 }
 
 void normalizeSphericalKeypoints(GpuMat &keypoints, Size imageSize, const SphericalLensParams &lensParams, cudaStream_t stream)

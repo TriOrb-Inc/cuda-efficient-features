28799a0bd9b64b2b52886384e16bbf86c91aa360
TOBETA <35523848+MasakazuTobeta@users.noreply.github.com>
2025-12-06 17:41:56 +0900

    Add CUDA error check for ORB kernel launch

--- modules/cuda_efficient_features/src/cuda_orb.cu
+++ modules/cuda_efficient_features/src/cuda_orb.cu
@@ -256,6 +256,7 @@ void computeORB(const GpuMat &integral, const GpuMat &keypoints, GpuMat &descrip
         computeORBKernel<<<grid, block, 0, stream>>>(integral.ptr<int>(), static_cast<int>(integral.step / sizeof(int)),
                 integral.cols - 1, integral.rows - 1, keypoints.ptr<float4>(), keypoints.rows, descriptors.ptr<unsigned char>(),
                 static_cast<int>(descriptors.step), scaleFactor, paramSize, patchSize.width, wrapHorizontal);
+        CUDA_CHECK(cudaGetLastError());
 }
 
 void normalizeSphericalKeypoints(GpuMat &keypoints, Size imageSize, const SphericalLensParams &lensParams, cudaStream_t stream)
